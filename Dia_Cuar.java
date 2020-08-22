a71b35c42a147cb56768990bbb1ce3ec5e2b0855
HenCor2019 <Henry200amaya@gmail.com>
2020-08-22 17:59:38 -0600

    Delete : Clean repository

--- Dia_Cuar.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Dia 3 de cuarentena");
-		System.out.println("Dia 5 de mi cuarentena");
-	}
-}
--- MyProject.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Inicio de nuevo proyecto");
-		System.out.println("Abriremos el 1 de abril");
-		System.out.println("Abriremos el 15 de abril");
-        System.out.println("Se necesitan operarios");
-	}
-}
